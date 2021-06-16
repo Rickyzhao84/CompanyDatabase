19390f6373862aba1fc361ef0ffce8d525c1dab6
ricky <pneumoniccoma@gmail.com>
2021-06-15 17:25:07 -0700

    Import request and received response

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import requests
+
+response = requests.get(
+    url = "https://en.wikipedia.org/wiki/Google",
+)
+
+print(response.status_code);
+
+print(response.content);
+
 #https://www.geeksforgeeks.org/web-scraping-from-wikipedia-using-python-a-complete-guide/
