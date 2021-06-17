90e1b56256498eab18699c9c27f8fd345afa9816
ricky <pneumoniccoma@gmail.com>
2021-06-16 20:59:45 -0700

    Used pandas to get table from wiki and also founders from table

--- main.py
+++ main.py
@@ -2,19 +2,41 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 response = requests.get(
-    url = "https://en.wikipedia.org/wiki/Google"
+url="https://en.wikipedia.org/wiki/Google"
 )
 table_name = 'wikitable sortable'
 soup = BeautifulSoup(response.content, 'html.parser')
 
-print(soup.find_all('table')[0].get_text())
-print(soup.find_all('table')[1].get_text())
 
 
+#Get the table that has Company Name
+def getCompanyName():
+    array = soup.find_all('table')
+    for key in array:
+        if ("Founders" in key.prettify()):
+            results = soup.find('td', class_='infobox-data')
+            return results.prettify()
+    return 0
+
+print(getCompanyName())
+
+def createTable(soup):
+    table = soup.find('table', {'class':"infobox vcard"})
+    df = pd.read_html(str(table))
+    df = pd.DataFrame(df[0])
+    print(df)
+    return getFounders(df)
+
+def getFounders(df):
+    for index, row in df.iterrows():
+        if ("Founders" in row[0] or "Founder" in row[0]):
+            print(row[1])
+            return row[1]
 
 
-print('\n\n')
+createTable(soup)
 
 
 def getResponseStatus():
--- main_test.py
+++ main_test.py
@@ -1,8 +1,52 @@
 import unittest
+import requests
+from bs4 import BeautifulSoup
 import main
 class TestMain(unittest.TestCase):
     def test_response_status_code(self):
         self.assertEqual(main.getResponseStatus(), 200)
+
+    def test_Google_founder(self):
+        response = requests.get(
+            url="https://en.wikipedia.org/wiki/Google"
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertTrue("Larry Page" in main.createTable(soup))
+
+    def test_Microsoft_founder(self):
+        response = requests.get(
+            url = "https://en.wikipedia.org/wiki/Microsoft"
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertTrue("Bill Gates" in main.createTable(soup))
+
+    # def test_Netflix_founder(self):
+    #     response = requests.get(
+    #         url = "https://en.wikipedia.org/wiki/Netflix"
+    #     )
+    #     soup = BeautifulSoup(response.content, 'html.parser')
+    #     self.assert_("Reed Hastings" in main.createTable(soup))
+
+    def test_Apple_founder(self):
+        response = requests.get(
+            url="https://en.wikipedia.org/wiki/Apple_Inc."
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertTrue("Steve Jobs" in main.createTable(soup))
+
+    def test_Amazon_founder(self):
+        response = requests.get(
+            url="https://en.wikipedia.org/wiki/Amazon_(company)"
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertTrue("Jeff Bezos" in main.createTable(soup))
+
+    def test_Stripe_founder(self):
+        response = requests.get(
+            url="https://en.wikipedia.org/wiki/Stripe_(company)"
+        )
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertTrue("John Collison" in main.createTable(soup))
 
 
 if __name__ == '__main__':
