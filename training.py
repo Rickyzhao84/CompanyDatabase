faf0d92135dcbaaef579252404df23b8a7d886f8
ricky <pneumoniccoma@gmail.com>
2021-06-29 17:26:23 -0700

    Adding BII

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 from logging import currentframe
 from flair.data import Sentence
 from flair.models import SequenceTagger
+from datetime import datetime
 import spacy
 
 nlp = spacy.load('en_core_web_sm', exclude=["tok2vec", "tagger", "parser", "lemmatizer", "ner", "transformer"])
@@ -26,29 +27,33 @@ for line in bingQueryFile:
             # print(sent.text)
             # newFile.write(sent.text)
             # newFile.write("\n")
+
             sent = Sentence(sent.text)
             tagger.predict(sent)
+            
             for token in sent.tokens:
                 if (token.text in currentCompany):
-                    print(token.text + "\t")
+
                     newFile.write(token.text)
-                    newFile.write("\n")
-                    token.add_tag("Organization", "I-Company")
+                    newFile.write("\t")
+                    token.add_tag("Organization", "B-Company")
                     newFile.write(token.get_tag("Organization").value)
                     newFile.write("\n")
-                
-                    print(token.get_tag("Organization").value)
+                    #If company is one word, it is B
+                    #If company is several, B-I-I-I until end of comp name
                 else:
-                    print(token.text + "\t")
-                    print(token.get_tag('ner').value)
-                    newFile.write(token.text + "\t")
-                    newFile.write("\n")
+
+                    newFile.write(token.text)
+                    newFile.write("\t")
                     newFile.write(token.get_tag('ner').value)
                     newFile.write("\n")
 
     if ("[QUERY]" in line.strip('\n')):
         companyName = line.split(":")[1]
         currentCompany = companyName
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print(current_time + ": processing " + companyName)
         # newFile.write(companyName)
         # newFile.write("\n")
     if ("https://" in line.strip('\n')):
